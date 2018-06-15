bbdc370cff4d6740f84a9cbcb71051a18284ab31
unknown <parilaos@hotmail.com>
2018-06-15 19:55:33 +0300

    list-part-1

--- src/Config/Brands.js
+++ src/Config/Brands.js
@@ -2,8 +2,8 @@ export const Brands = {
     'Κινητό' :['Huawei','Samsung','Apple', 'Sony', 'Nokia', 'Xiaomi' , 'Asus', 'Cubot', 'HTC', 'Lenovo' ,'LG', 'MLS','ZTE'],
     'Laptop' : [ 'Acer', 'Apple', 'Asus', 'Dell','HP', 'Lenovo', 'MSI' , 'Samsung', 'Toshiba', 'ΤurboX'],
     'Σταθερός Η/Υ' : [ 'Apple' ,'Dell' , 'HP' , 'Lenovo' ,'Asus' , 'MSI' , 'Fujitsu', 'Acer', 'TurboX', 'Quest', 'Άλλο'],
-    'Tablet' : [],
-    'Εκτυπωτής' : [] , 
-    'Game Console' : []
+    'Tablet' : ['Acer','Apple','Archos','Asus','Bitmore','eStar','HP','Huawei','Lenovo','Microsoft','MLS','Samsung'],
+    'Εκτυπωτής' : ['Canon','Epson','HP','Kyocera','Lexmark','Ricoh','Samsung','Xerox'] , 
+    'Game Console' : ['Microsoft','Nintendo','Sony']
 
 };
--- src/Config/Models.js
+++ src/Config/Models.js
@@ -1,5 +1,20 @@
-export const Models = {
-     'Samsung' : ['Galaxy Note 3','Galaxy Note 3 Neo','Galaxy Note 4','Galaxy Note Edge','Galaxy Note 5','Galaxy Note 8','Galaxy S III','Galaxy S III Neo','Galaxy S III Mini','Galaxy S4','Galaxy S4 Mini','Galaxy S5','Galaxy S5 Mini','Galaxy S5 Neo','Galaxy S6','Galaxy S6 Edge','Galaxy S6 Edge+','Galaxy S7','Galaxy S7 Edge','Galaxy S8','Galaxy S8+','Galaxy S9','Galaxy S9+','Galaxy Alpha', 'Galaxy A3 (2015)','Galaxy A3 (2016)','Galaxy A3 (2017)','Galaxy A5 (2015)','Galaxy A5 (2016)', 'Galaxy A5 (2017)',
- 'Galaxy J1','Galaxy J5','Galaxy J7','Galaxy J2'], 
-  'Apple': ['iPhone 5S','iPhone 6','iPhone 6 Plus','iPhone 6S','iPhone 6S Plus','iPhone SE','iPhone 7','iPhone 7 Plus','iPhone 8','iPhone 8 Plus','iPhone X ','iPhone 3G','iPhone 3GS','iPhone 4','iPhone 4S','iPhone 5','iPhone 5C']
-};
+// export const Brands = {
+//   'Κινητό' :['Huawei','Samsung','Apple', 'Sony', 'Nokia', 'Xiaomi' , 'Asus', 'Cubot', 'HTC', 'Lenovo' ,'LG', 'MLS','ZTE'],
+//   'Laptop' : [ 'Acer', 'Apple', 'Asus', 'Dell','HP', 'Lenovo', 'MSI' , 'Samsung', 'Toshiba', 'ΤurboX'],
+//   'Σταθερός Η/Υ' : [ 'Apple' ,'Dell' , 'HP' , 'Lenovo' ,'Asus' , 'MSI' , 'Fujitsu', 'Acer', 'TurboX', 'Quest', 'Άλλο'],
+//   'Tablet' : ['Acer','Apple','Archos','Asus','Bitmore','eStar','HP','Huawei','Lenovo','Microsoft','MLS','Samsung'],
+//   'Εκτυπωτής' : ['Canon','Epson','HP','Kyocera','Lexmark','Ricoh','Samsung','Xerox'] , 
+//   'Game Console' : ['Microsoft','Nintendo','Sony']
+
+// };
+
+// export const phone = {
+//      'Samsung' : ['Galaxy Note 3','Galaxy Note 3 Neo','Galaxy Note 4','Galaxy Note Edge','Galaxy Note 5','Galaxy Note 8','Galaxy S III','Galaxy S III Neo','Galaxy S III Mini','Galaxy S4','Galaxy S4 Mini','Galaxy S5','Galaxy S5 Mini','Galaxy S5 Neo','Galaxy S6','Galaxy S6 Edge','Galaxy S6 Edge+','Galaxy S7','Galaxy S7 Edge','Galaxy S8','Galaxy S8+','Galaxy S9','Galaxy S9+','Galaxy Alpha', 'Galaxy A3 (2015)','Galaxy A3 (2016)','Galaxy A3 (2017)','Galaxy A5 (2015)','Galaxy A5 (2016)', 'Galaxy A5 (2017)',
+//   'Galaxy J1','Galaxy J5','Galaxy J7','Galaxy J2'], 
+//   'Apple': ['iPhone 5S','iPhone 6','iPhone 6 Plus','iPhone 6S','iPhone 6S Plus','iPhone SE','iPhone 7','iPhone 7 Plus','iPhone 8','iPhone 8 Plus','iPhone X ','iPhone 3G','iPhone 3GS','iPhone 4','iPhone 4S','iPhone 5','iPhone 5C']
+// };
+
+
+// export const laptop = {}
+
+
