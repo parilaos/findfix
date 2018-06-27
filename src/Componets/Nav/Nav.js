db0cd90b92cd9972caf7cfb91b800da3c3307a09
parilaos <parilaos@hotmail.com>
2018-06-27 23:16:03 +0300

    design day one

--- src/Componets/Nav/Nav.js
+++ src/Componets/Nav/Nav.js
@@ -7,7 +7,7 @@ const nav =(props) => (
 
 <nav className="navbar navbar-expand-lg navbar-light bg-light  justify-content-between">
   <div className="container">
-  <Link to='/' className="navbar-brand">FindFix</Link>
+  <Link to='/' className="navbar-brand"><img src="../../img/logo.png" width="30"  className="d-inline-block" alt="FindFix"/>FindFix</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
--- src/Componets/UI/input/MainInput.js
+++ src/Componets/UI/input/MainInput.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const mainInput = (props) => {
     return (
-        <div>
-            <p className="lead">{props.text}</p>
-                <input type="text" className="form-control" id={props.id}  
-                 onChange={props.changed}
-                 placeholder={props.placeholder} />
+        <div className="input-group mb-3">
+            <input type="text" className="form-control" aria-label="Περιοχή ή Τ.Κ." aria-describedby="basic-addon2"
+            id={props.id}  
+            onChange={props.changed}
+            placeholder={props.placeholder} />
+            <div className="input-group-append">
+                <button className="btn btn-findfix" type="button">Επόμενο</button>
+            </div>
         </div>
     );
 }
