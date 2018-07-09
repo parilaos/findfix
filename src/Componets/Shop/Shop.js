ce4ef8962feafe11377e95c0cc94a2b974656d7c
parilaos <parilaos@hotmail.com>
2018-07-09 20:10:49 +0300

    results

--- src/Componets/Shop/Shop.js
+++ src/Componets/Shop/Shop.js
@@ -3,22 +3,35 @@ import Rating from '../UI/Rating/Rating';
 
 const shop = ( props ) => (
     <div className="ff-card">
-         <div className="ff-card-body">
+         {/* <div className="ff-card-body">
             <div className="ff-card-title"> 
                 <div className="float-left">
-                    {props.name} 
+                   
                 </div>
                 <div className="clearfix float-right">
-                    <Rating />
+                    
                 </div>
             </div>
             <div className="ff-card-text">
                 <div className="float-left">
-                    {props.route}  {props.street_number} <br /> {props.locality} , {props.postal_code}
+                    
                 </div>
-                <div className="clearfix float-right">
-                    <button className="btn-service">θέλω επισκευή</button>
-                </div>
+               
+            </div>
+        </div> */}
+        <div className="d-flex flex-row">
+            <div className="shop_logo">
+                <img src="../img/logo.png" alt="shop logo" height="80"/>
+            </div>
+            <div className="shop_info">
+                <div className="d-flex flex-column">
+                    <div className="p-2 shop_title">{props.name}</div>
+                    <div className="p-2">  <Rating /> </div>
+                    <div className="p-2 shop_address">{props.route} {props.street_number} , {props.locality} , {props.postal_code}  </div>
+                </div>  
+            </div>
+            <div>
+                <button className="btn-service">θέλω επισκευή</button>
             </div>
         </div>
     </div>
